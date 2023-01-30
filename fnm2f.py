2cc4184aceba9ad002878826b083c226560667c3
Yifan Li <yifanl0716@gmail.com>
2023-01-30 08:02:59 -0500

    added batch loop for fnm2f

--- fnm2f.py
+++ fnm2f.py
@@ -18,21 +18,28 @@ forcenms = np.load(directory+"forcenms"+str(rank)+".npy")
 print("forcenms loaded")
 nsamp = forcenms.shape[0]
 natom = forcenms.shape[1]
+forces = np.zeros([nbeads, nsamp, natom, 3])
 nbatch = 5000
-forcenms_batch = forcenms[:nbatch]
-nsamp = nbatch
-fnms = np.array(comm.gather(forcenms_batch, root=0))
+nloop = int(nsamp/nbatch)
+for iloop in range(nloop):
+  forcenms_batch = forcenms[iloop*nbatch:(iloop+1)*nbatch]
+# nsamp = nbatch
+  fnms = np.array(comm.gather(forcenms_batch, root=0))
 
-if rank==0:
-  fnms_complex = np.zeros([nmodes + 1, nsamp, natom, 3], complex)
-  fnms_complex[0] = fnms[0]
-  fnms_complex[nmodes] = fnms[nmodes]
+  if rank==0:
+    fnms_complex = np.zeros([nmodes + 1, nbatch, natom, 3], complex)
+    fnms_complex[0] = fnms[0]
+    fnms_complex[nmodes] = fnms[nmodes]
 #   print(fnms.shape)
 #   print(fnms_complex.shape)
-  fnms_complex[1:nmodes].real = fnms[1:nmodes]
-  fnms_complex[1:nmodes].imag = fnms[nbeads-1:nmodes:-1]
-  fnms_complex[1:nmodes] /= 2**0.5
-  forces = np.fft.irfft(fnms_complex, n=nbeads, axis=0, norm="ortho")
+    fnms_complex[1:nmodes].real = fnms[1:nmodes]
+    fnms_complex[1:nmodes].imag = fnms[nbeads-1:nmodes:-1]
+    fnms_complex[1:nmodes] /= 2**0.5
+    forces_batch = np.fft.irfft(fnms_complex, n=nbeads, axis=0, norm="ortho")
+    forces[:, iloop*nbatch:(iloop+1)*nbatch] = forces_batch
+  
+
+if rank==0:  
   for ib in range(nbeads):
     np.save(directory+"forces"+str(ib)+".npy", forces[ib])
   
