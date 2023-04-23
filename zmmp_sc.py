7728d35085d97f5519e56b6304efa942d2b30251
Yifan Li <yifanl0716@gmail.com>
2023-04-23 16:34:50 -0700

    update Zmmp^SC

--- zmmp_sc.py
+++ zmmp_sc.py
@@ -5,7 +5,7 @@ import argparse
 import copy
 from time import time
 
-dp = DeepPot("/scratch/gpfs/yifanl/Work/pimd/h2o/scan_prl_lfz/frozen_model_compressed.pb")
+dp = DeepPot("/pscratch/sd/y/yifanl/Work/h2o/pimd/scan_natcomm/compress.pb")
 comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 nbeads = comm.size
@@ -28,23 +28,21 @@ omega_np = nbeads / (hbar * beta) * mvv2e**0.5
 ndiscard = 0000
 #nsamp = 10001
 nsamp = args.nsamp
-nbatch=10
+nbatch=20
 nloop=int(nsamp/nbatch)
 zmmp_sc1 = np.zeros(nloop*nbatch)
 zmmp_sc = np.zeros(nloop*nbatch)
 zmmpO_sc1 = np.zeros(nloop*nbatch)
 zmmpO_sc = np.zeros(nloop*nbatch)
 se = np.zeros(nloop*nbatch)
-directory = "../h2o/task"+str(args.id)+"/"
+directory = "../fractionation/task"+str(args.id)+"/"
 coords = np.load(directory+"coords"+str(rank)+".npy")[ndiscard:ndiscard+nsamp]
 coords_unmap = np.load(directory+"coords_unmap"+str(rank)+".npy")[ndiscard:ndiscard+nsamp]
 natom = coords.shape[1]
-# print("natom = %d"%(natom))
 
 types=np.load(directory+'types'+str(rank)+'.npy')[ndiscard:ndiscard+nsamp]
 idx = np.arange(natom)
-#idx_O = np.arange(0, natom, 3)
-#idx_H = np.delete(idx, idx_O)
+
 idx_O = np.where(types[0]==1)[0]
 idx_H = np.where(types[0]==2)[0]
 
@@ -60,16 +58,14 @@ mO18 = 17.99916
 mratio = np.zeros(natom)
 mratio[idx_O] = (mO18/mO16)**0.5
 mratio[idx_H] = (mD/mH)**0.5
-mratio_bc = np.diag(mratio)+np.ones([natom, natom])-np.eye(natom)
-mratio_xc_bc = np.diag(1-mratio)+np.ones([natom, natom])-np.eye(natom)
 
 atype = np.zeros(natom)
 atype[idx_H] = 1
 
 cells = np.load(directory+"cells"+str(rank)+".npy")[ndiscard:ndiscard+nsamp]
-# if rank == 0:
+
 e_unscaled = np.load(directory+"pote"+str(rank)+".npy")[ndiscard:ndiscard+nsamp]
-ros(nloop*nbatch)
+e_sc_H = np.zeros(nloop*nbatch)
 e_sc_O = np.zeros(nloop*nbatch)
 
 t1 = time()
@@ -83,33 +79,14 @@ for iloop in range(nloop):
   else:
     xc = None
   xc = comm.bcast(xc, root=0)
-  # coords_sc_batch = coords_unmap_batch[:, None, :, :] * mratio_bc[None, :, :, None] + xc[:, None, :, :] * mratio_xc_bc[None, :, :, None]
-  # coords_sc_batch_feed = coords_sc_batch.reshape(nbatch*natom, natom*3)
   coords_sc_O_batch = copy.deepcopy(coords_unmap_batch)
-  # print(coords_sc_O_batch[:, idx_O[0]])
   coords_sc_O_batch[:, idx_O[0]] *= mratio[idx_O[0]]
-  # print("after xmratio, ")
-  # print(coords_sc_O_batch[:, idx_O[0]])
   coords_sc_O_batch[:, idx_O[0]] += ((1-mratio[idx_O[0]]) * xc[:, idx_O[0]])
-  # print("finally, ")
-  # print(coords_sc_O_batch[:, idx_O[0]])
 
   coords_sc_H_batch = copy.deepcopy(coords_unmap_batch)
   coords_sc_H_batch[:, idx_H[0]] *= mratio[idx_H[0]]
   coords_sc_H_batch[:, idx_H[0]] += ((1-mratio[idx_H[0]]) * xc[:, idx_H[0]])
 
-  # print("xO:")
-  # print(coords_unmap_batch[:, idx_O[0]])
-  # print("xO, scaled:")
-  # print(coords_sc_O_batch[:, idx_O[0]])
-
-  # print("xH:")
-  # print(coords_unmap_batch[:, idx_H[0]])
-  # print("xH, scaled:")
-  # print(coords_sc_H_batch[:, idx_H[0]])
-
-  e, f, v = dp.eval(coords_unmap_batch.reshape(nbatch, natom*3), cells_batch, atype)
-  e_unscaled[samp_idx] = e.flatten()
   e_sc_O_batch, f_O_sc, v_O_sc = dp.eval(coords_sc_O_batch.reshape(nbatch, natom*3), cells_batch, atype)
   e_sc_O[samp_idx] = e_sc_O_batch.flatten()
   e_sc_H_batch, f_H_sc, v_H_sc = dp.eval(coords_sc_H_batch.reshape(nbatch, natom*3), cells_batch, atype)
@@ -130,25 +107,3 @@ if rank == 0:
   zmmp_sc_H = np.exp(beta_np*(e_unscaled_sum-e_sc_H_sum))
   np.save(directory+"zmmp_sc_O.npy", zmmp_sc_O)
   np.save(directory+"zmmp_sc_H.npy", zmmp_sc_H)
-  # np.save("e_sc_O_sum.npy", e_sc_O_sum)
-  # np.save("e_sc_H_sum.npy", e_sc_H_sum)
-  # cells_batch_feed = np.array([cells_batch for i in range(natom)]).reshape(nbatch*natom, 9)
-  # e_sc = np.zeros(nbatch*natom)
-  # for jloop in range(natom):
-  #   minibatch_idx=np.arange(jloop*nbatch, (jloop+1)*nbatch)
-  #   coords_sc_minibatch_feed = coords_sc_batch_feed[minibatch_idx]
-  #   cells_minibatch_feed = cells_batch_feed[minibatch_idx]
-  #   e_sc_minibatch, f_sc_minibatch, v_sc_minibatch = dp.eval(coords_sc_minibatch_feed, cells_minibatch_feed, atype)
-  #   e_sc[minibatch_idx] = e_sc_minibatch.flatten()
-  #   t3 = time()
-  #   print("Atom %d of %d, loop %d, time: %.4f s.\n"%(jloop, natom, iloop, t3-t1))
-  # # e_sc, f_sc, v_sc = dp.eval(coords_sc_batch_feed, cells_batch_feed, atype)
-  # t2 = time()
-  # print("Loop %d of %d, time: %.4f s.\n"%(iloop, nloop, t2-t1))
-#   e_sc_beads = np.array(comm.gather(e_sc, root=0))
-#   if rank == 0:
-#     e_sc_sum = e_sc_beads.sum(axis=0)
-#     e_sc_atoms[samp_idx] = e_sc_sum
-
-# if rank == 0:
-#   np.save("e_sc_atoms.npy", e_sc_atoms)
